8262c4ecd95c270ca698809a1637f38764bbc2d2
Rafael Barbosa <rafaeldacostabarbosa@gmail.com>
2025-04-19 16:04:53 -0300

    test: include mocks in test cases

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,14 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { UsersMapper } from './mappers/users.mapper';
+import { IUsersRepository } from './users.repository.interface';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let usersRepositoryMock: Partial<IUsersRepository>;
 
   beforeEach(async () => {
+    usersRepositoryMock = {
+      create: jest.fn(),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        UsersMapper,
+        {
+          provide: 'IUsersRepository',
+          useValue: usersRepositoryMock,
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,12 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { IUsersRepository } from './users.repository.interface';
+import { UsersMapper } from './mappers/users.mapper';
 
 describe('UsersService', () => {
   let service: UsersService;
+  let usersRepositoryMock: Partial<IUsersRepository>;
 
   beforeEach(async () => {
+    usersRepositoryMock = {
+      create: jest.fn(),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        UsersMapper,
+        {
+          provide: 'IUsersRepository',
+          useValue: usersRepositoryMock,
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
